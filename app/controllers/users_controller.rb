49689a54313e1d25a4a20c6bfb530312d047d729
maquino96 <matthewaquino96@gmail.com>
2021-03-26 12:00:43 -0400

    login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+    def signup 
+        @user = User.create(user_params)
+        
+        if @user.valid?
+            render json: @user
+        else 
+            render json: false
+        end 
+    end 
+
+    def login 
+        @user = User.find_by(user_params)
+
+        if @user 
+                render json: @user
+        else
+                render json: false
+        end  
+    end 
+
+    private 
+
+    def user_params
+        params.permit(:name, :password)
+    end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :listings
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  post '/users/signup', to: 'users#signup' 
+  post '/users/login', to: 'users#login'
+
 end
